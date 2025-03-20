daeca88b588f9ed08ed15eaf7f9d26b4b9ff41b1
Kinar R <kinar.ravishankar@codeandtheory.com>
2025-03-20 11:56:02 +0530

    Add the remaining text generation JS samples

--- javascript/text_generation.test.js
+++ javascript/text_generation.test.js
@@ -15,13 +15,81 @@
  * limitations under the License.
  */
 
-import { textGenTextOnlyPrompt } from "./text_generation.js";
 import assert from "node:assert";
 import { test, describe } from "node:test";
+import {
+  textGenTextOnlyPrompt,
+  textGenTextOnlyPromptStreaming,
+  textGenMultimodalOneImagePrompt,
+  textGenMultimodalOneImagePromptStreaming,
+  textGenMultimodalMultiImagePrompt,
+  textGenMultimodalMultiImagePromptStreaming,
+  textGenMultimodalAudio,
+  textGenMultimodalAudioStreaming,
+  textGenMultimodalVideoPrompt,
+  textGenMultimodalVideoPromptStreaming,
+  textGenMultimodalPdf,
+  textGenMultimodalPdfStreaming,
+} from "./text_generation.js";
 
 describe("text_generation", () => {
   test("textGenTextOnlyPrompt", async () => {
     const text = await textGenTextOnlyPrompt();
+    assert.ok(text.length > 0);
+  });
+
+  test("textGenTextOnlyPromptStreaming", async () => {
+    const text = await textGenTextOnlyPromptStreaming();
+    assert.ok(text.length > 0);
+  });
+
+  test("textGenMultimodalOneImagePrompt", async () => {
+    const text = await textGenMultimodalOneImagePrompt();
+    assert.ok(text.length > 0);
+  });
+
+  test("textGenMultimodalOneImagePromptStreaming", async () => {
+    const text = await textGenMultimodalOneImagePromptStreaming();
+    assert.ok(text.length > 0);
+  });
+
+  test("textGenMultimodalMultiImagePrompt", async () => {
+    const text = await textGenMultimodalMultiImagePrompt();
+    assert.ok(text.length > 0);
+  });
+
+  test("textGenMultimodalMultiImagePromptStreaming", async () => {
+    const text = await textGenMultimodalMultiImagePromptStreaming();
+    assert.ok(text.length > 0);
+  });
+
+  test("textGenMultimodalAudio", async () => {
+    const text = await textGenMultimodalAudio();
+    assert.ok(text.length > 0);
+  });
+
+  test("textGenMultimodalAudioStreaming", async () => {
+    const text = await textGenMultimodalAudioStreaming();
+    assert.ok(text.length > 0);
+  });
+
+  test("textGenMultimodalVideoPrompt", async () => {
+    const text = await textGenMultimodalVideoPrompt();
+    assert.ok(text.length > 0);
+  });
+
+  test("textGenMultimodalVideoPromptStreaming", async () => {
+    const text = await textGenMultimodalVideoPromptStreaming();
+    assert.ok(text.length > 0);
+  });
+
+  test("textGenMultimodalPdf", async () => {
+    const text = await textGenMultimodalPdf();
+    assert.ok(text.length > 0);
+  });
+
+  test("textGenMultimodalPdfStreaming", async () => {
+    const text = await textGenMultimodalPdfStreaming();
     assert.ok(text.length > 0);
   });
 });
